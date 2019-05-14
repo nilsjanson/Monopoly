4e28582364b60d1e03b862e100965bf2862a57eb
nilsjanson <nils.janson@th-bingen.de>
2019-05-14 22:47:55 +0200

    Paar Klassen hinzugefügt

--- Eclipse/Monopoly/src/model/Board.java
+++ Eclipse/Monopoly/src/model/Board.java
@@ -1,5 +1,18 @@
 package model;
 
 public class Board {
+	
+	private model.Field[] cards;
+	
+	Board() {
+	
+	}
+	
+	public void init() {
+		cards = new Field[40];
+		cards[0] = new Go(0,"Start");
+		cards[1] = new Card("Badtstraﬂe", 1, 60, mortgage, street, rentalFee)
+		
+	}
 
 }
