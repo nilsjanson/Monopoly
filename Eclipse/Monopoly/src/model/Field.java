348f8420735e233c47cc21fff8b192673b515b51
FFX721 <49677342+FFX721@users.noreply.github.com>
2019-05-16 14:12:15 +0200

    Änderung an der Superklasse, Icon (ImageView)

--- Eclipse/Monopoly/src/model/Field.java
+++ Eclipse/Monopoly/src/model/Field.java
@@ -1,18 +1,27 @@
 package model;
 
+import java.io.FileNotFoundException;
+
+import javafx.scene.image.ImageView;
+
 public class Field {
-	
+
 	private int location;
 	private String name;
 	private boolean isStreet;
+
+	public ImageView icon;
+
+	public ImageView getIcon() throws FileNotFoundException {
+		return icon;
+	}
 
 	public Field(int location, String name, boolean isStreet) {
 		super();
 		this.location = location;
 		this.name = name;
 		this.isStreet = isStreet;
+		icon = new ImageView(Go.class.getResource("/icons/"+name+".gif").toExternalForm());
 	}
-	
-
 
 }
