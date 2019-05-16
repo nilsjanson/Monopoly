b83c238fa56a7d505814037017bdf2d1b78212cc
FFX721 <49677342+FFX721@users.noreply.github.com>
2019-05-16 14:45:26 +0200

    Hinzufuegen des ersten Entwurfes des Spielfeldes

--- Eclipse/Monopoly/src/model/Board.java
+++ Eclipse/Monopoly/src/model/Board.java
@@ -9,50 +9,50 @@ public class Board {
 	}
 
 
+//	 /*
 	public void init() {
 		cards = new Field[40];
 		cards[0] = new Go(0, "Start");
 		 cards[1] = new Card("Kesselhaus", 1, 60, mortgage, street, rentalFee);
-		cards[2] = new Card(" ",1,60,mortage,street,rentalFee);
-		cards[3] = new Card(" ",1,60,mortage,street,rentalFee);
-		cards[4] = new Card(" ",1,60,mortage,street,rentalFee);
-		cards[5] = new Card(" ",1,60,mortage,street,rentalFee);
-		cards[6] = new Card(" ",1,60,mortage,street,rentalFee);
-		cards[7] = new Card(" ",1,60,mortage,street,rentalFee);
-		cards[8] = new Card(" ",1,60,mortage,street,rentalFee);
-		cards[9] = new Card(" ",1,60,mortage,street,rentalFee);
-		cards[10] = new Card(" ",1,60,mortage,street,rentalFee);
-		cards[11] = new Card(" ",1,60,mortage,street,rentalFee);
-		cards[12] = new Card(" ",1,60,mortage,street,rentalFee);
-		cards[13] = new Card(" ",1,60,mortage,street,rentalFee);
-		cards[14] = new Card(" ",1,60,mortage,street,rentalFee);
-		cards[15] = new Card(" ",1,60,mortage,street,rentalFee);
-		cards[16] = new Card(" ",1,60,mortage,street,rentalFee);
-		cards[17] = new Card(" ",1,60,mortage,street,rentalFee);
-		cards[18] = new Card(" ",1,60,mortage,street,rentalFee);
-		cards[19] = new Card(" ",1,60,mortage,street,rentalFee);
-		cards[20] = new Card(" ",1,60,mortage,street,rentalFee);
-		cards[21] = new Card(" ",1,60,mortage,street,rentalFee);
-		cards[22] = new Card(" ",1,60,mortage,street,rentalFee);
-		cards[23] = new Card(" ",1,60,mortage,street,rentalFee);
-		cards[24] = new Card(" ",1,60,mortage,street,rentalFee);
-		cards[25] = new Card(" ",1,60,mortage,street,rentalFee);
-		cards[26] = new Card(" ",1,60,mortage,street,rentalFee);
-		cards[27] = new Card(" ",1,60,mortage,street,rentalFee);
-		cards[28] = new Card(" ",1,60,mortage,street,rentalFee);
-		cards[29] = new Card(" ",1,60,mortage,street,rentalFee);
-		cards[30] = new Card(" ",1,60,mortage,street,rentalFee);
-		cards[31] = new Card(" ",1,60,mortage,street,rentalFee);
-		cards[32] = new Card(" ",1,60,mortage,street,rentalFee);
-		cards[33] = new Card(" ",1,60,mortage,street,rentalFee);
-		cards[34] = new Card(" ",1,60,mortage,street,rentalFee);
-		cards[35] = new Card(" ",1,60,mortage,street,rentalFee);
-		cards[36] = new Card(" ",1,60,mortage,street,rentalFee);
-		cards[37] = new Card(" ",1,60,mortage,street,rentalFee);
-		cards[38] = new Card(" ",1,60,mortage,street,rentalFee);
-		cards[39] = new Card(" ",1,60,mortage,street,rentalFee);
-		cards[40] = new Card(" ",1,60,mortage,street,rentalFee);
+		cards[2] = new Card("Asta Kiste",2,); // String name, int location, int cost, int mortgage, Street street, int[] rentalFee
+		cards[3] = new Card("Asta Buero",1,60,mortage,street,rentalFee);
+		cards[4] = new Card("Semester Beitrag",1,60,mortage,street,rentalFee);
+	cards[5] = new Card("Bingen Bahnhof",5,60,mortage,street,rentalFee);
+	cards[6] = new Card("Pflangenlabor",6,60,mortage,street,rentalFee);
+	cards[7] = new Card("Chance",7,60,mortage,street,rentalFee);
+	cards[8] = new Card("Gewaechshaus",8,60,mortage,street,rentalFee);
+	cards[9] = new Card("Demonstrationsfeld",9,60,mortage,street,rentalFee);
+	cards[9] = new Card("Mathe Vorlesung",10,60,mortage,street,rentalFee);
+	cards[10] = new Card("Abgastest",11,60,mortage,street,rentalFee);
+	cards[11] = new Card("Rechenzentrum",12,60,mortage,street,rentalFee);
+	cards[12] = new Card("Fahrzeuglabor",13,60,mortage,street,rentalFee);
+	cards[13] = new Card("Solartankstelle",14,60,mortage,street,rentalFee);
+	cards[14] = new Card("Kreuznach Bahnhof",15,60,mortage,street,rentalFee);
+	cards[15] = new Card("",16,60,mortage,street,rentalFee);
+	cards[16] = new Card("Asta Kiste",17,60,mortage,street,rentalFee);
+	cards[17] = new Card("",18,60,mortage,street,rentalFee);
+	cards[18] = new Card("",19,60,mortage,street,rentalFee);
+	cards[19] = new Card("Bafoeg Amt",20,60,mortage,street,rentalFee);
+	cards[20] = new Card("Netzwerklabor",21,60,mortage,street,rentalFee);
+	cards[21] = new Card("Chance",22,60,mortage,street,rentalFee);
+	cards[21] = new Card("PC-Pool R1-236",23,60,mortage,street,rentalFee);
+	cards[22] = new Card("PC-Pool R1-237",24,60,mortage,street,rentalFee);
+	cards[23] = new Card("Mainz Bahnhof",25,60,mortage,street,rentalFee);
+	cards[24] = new Card("",26,60,mortage,street,rentalFee);
+	cards[25] = new Card("",27,60,mortage,street,rentalFee);
+	cards[26] = new Card("Bingen Impulse",28,60,mortage,street,rentalFee);
+	cards[28] = new Card("",29,60,mortage,street,rentalFee);
+	cards[31] = new Card("Ab in die Mathe Vorlesung",30,60,mortage,street,rentalFee);
+	cards[31] = new Card("",31,60,mortage,street,rentalFee);
+	cards[32] = new Card("",32,60,mortage,street,rentalFee);
+	cards[33] = new Card("Asta Kiste",33,60,mortage,street,rentalFee);
+	cards[34] = new Card("",34,60,mortage,street,rentalFee);
+	cards[35] = new Card("Worms Bahnhof",35,60,mortage,street,rentalFee);
+	cards[36] = new Card("Chance",36,60,mortage,street,rentalFee);
+	cards[37] = new Card("Mensa",37,60,mortage,street,rentalFee);
+	cards[38] = new Card("Schwarz gefahren",38,60,mortage,street,rentalFee);
+	cards[39] = new Card("Bibliothek",39,60,mortage,street,rentalFee);
 
 	}
-
+//	 */
 }
