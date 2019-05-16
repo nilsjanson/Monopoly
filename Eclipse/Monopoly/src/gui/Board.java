e3a379bfc139e30db929a7da95c65d25de6b6c5f
FFX721 <49677342+FFX721@users.noreply.github.com>
2019-05-16 14:08:13 +0200

    Hinzufügen eines Icons + der JavaFx Library

--- Eclipse/Monopoly/src/gui/Board.java
+++ Eclipse/Monopoly/src/gui/Board.java
@@ -1,7 +1,5 @@
 package gui;
 
-import java.awt.BorderLayout;
-
 public class Board {
 
 	
--- Eclipse/Monopoly/src/model/Go.java
+++ Eclipse/Monopoly/src/model/Go.java
@@ -1,6 +1,15 @@
 package model;
 
+import java.io.FileNotFoundException;
+import javafx.scene.image.ImageView;
+
 public class Go extends Field {
+	
+public ImageView icon = new ImageView(Go.class.getResource("/icons/go.gif").toExternalForm());
+	
+	public ImageView getIcon() throws FileNotFoundException {
+		return icon;
+	}
 
 	public Go(int location, String name) {
 		super(location, name, false);
