bfeff6bf5d7c05699d36a1a4bc34c3638d90b549
FFX721 <49677342+FFX721@users.noreply.github.com>
2019-05-16 15:41:48 +0200

    hinzufuegen der Strassenklassen

---
+++ Eclipse/Monopoly/src/streets/AstaBuero.java
@@ -0,0 +1,10 @@
+package streets;
+
+public class AstaBuero extends Street {
+
+	public AstaBuero(int location, String name) {
+		super(location, name);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ Eclipse/Monopoly/src/streets/Kesselhaus.java
@@ -0,0 +1,10 @@
+package streets;
+
+public class Kesselhaus extends Street {
+
+	public Kesselhaus(int location, String name) {
+		super(location, name);
+		// TODO Auto-generated constructor stub
+	}
+
+}
