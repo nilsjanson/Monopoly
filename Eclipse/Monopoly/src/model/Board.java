b61ebe0260a02a0a93f6a25a0a6b3f38694b6e52
FFX721 <49677342+FFX721@users.noreply.github.com>
2019-05-16 14:24:11 +0200

    Hinzufuegen der einzelnen Eintraege

--- Eclipse/Monopoly/src/model/Board.java
+++ Eclipse/Monopoly/src/model/Board.java
@@ -1,18 +1,58 @@
 package model;
 
 public class Board {
-	
+
 	private model.Field[] cards;
-	
+
 	Board() {
-	
+
 	}
-	
+
+
 	public void init() {
 		cards = new Field[40];
-		cards[0] = new Go(0,"Start");
-		cards[1] = new Card("Badtstraﬂe", 1, 60, mortgage, street, rentalFee)
-		
+		cards[0] = new Go(0, "Start");
+		 cards[1] = new Card("Kesselhaus", 1, 60, mortgage, street, rentalFee);
+		cards[2] = new Card(" ",1,60,mortage,street,rentalFee);
+		cards[3] = new Card(" ",1,60,mortage,street,rentalFee);
+		cards[4] = new Card(" ",1,60,mortage,street,rentalFee);
+		cards[5] = new Card(" ",1,60,mortage,street,rentalFee);
+		cards[6] = new Card(" ",1,60,mortage,street,rentalFee);
+		cards[7] = new Card(" ",1,60,mortage,street,rentalFee);
+		cards[8] = new Card(" ",1,60,mortage,street,rentalFee);
+		cards[9] = new Card(" ",1,60,mortage,street,rentalFee);
+		cards[10] = new Card(" ",1,60,mortage,street,rentalFee);
+		cards[11] = new Card(" ",1,60,mortage,street,rentalFee);
+		cards[12] = new Card(" ",1,60,mortage,street,rentalFee);
+		cards[13] = new Card(" ",1,60,mortage,street,rentalFee);
+		cards[14] = new Card(" ",1,60,mortage,street,rentalFee);
+		cards[15] = new Card(" ",1,60,mortage,street,rentalFee);
+		cards[16] = new Card(" ",1,60,mortage,street,rentalFee);
+		cards[17] = new Card(" ",1,60,mortage,street,rentalFee);
+		cards[18] = new Card(" ",1,60,mortage,street,rentalFee);
+		cards[19] = new Card(" ",1,60,mortage,street,rentalFee);
+		cards[20] = new Card(" ",1,60,mortage,street,rentalFee);
+		cards[21] = new Card(" ",1,60,mortage,street,rentalFee);
+		cards[22] = new Card(" ",1,60,mortage,street,rentalFee);
+		cards[23] = new Card(" ",1,60,mortage,street,rentalFee);
+		cards[24] = new Card(" ",1,60,mortage,street,rentalFee);
+		cards[25] = new Card(" ",1,60,mortage,street,rentalFee);
+		cards[26] = new Card(" ",1,60,mortage,street,rentalFee);
+		cards[27] = new Card(" ",1,60,mortage,street,rentalFee);
+		cards[28] = new Card(" ",1,60,mortage,street,rentalFee);
+		cards[29] = new Card(" ",1,60,mortage,street,rentalFee);
+		cards[30] = new Card(" ",1,60,mortage,street,rentalFee);
+		cards[31] = new Card(" ",1,60,mortage,street,rentalFee);
+		cards[32] = new Card(" ",1,60,mortage,street,rentalFee);
+		cards[33] = new Card(" ",1,60,mortage,street,rentalFee);
+		cards[34] = new Card(" ",1,60,mortage,street,rentalFee);
+		cards[35] = new Card(" ",1,60,mortage,street,rentalFee);
+		cards[36] = new Card(" ",1,60,mortage,street,rentalFee);
+		cards[37] = new Card(" ",1,60,mortage,street,rentalFee);
+		cards[38] = new Card(" ",1,60,mortage,street,rentalFee);
+		cards[39] = new Card(" ",1,60,mortage,street,rentalFee);
+		cards[40] = new Card(" ",1,60,mortage,street,rentalFee);
+
 	}
 
 }
