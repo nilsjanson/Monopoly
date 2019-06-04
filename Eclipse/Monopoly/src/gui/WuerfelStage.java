f4f76af3795fb60c7f1c34aaebef63eda8f237e5
FFX721 <49677342+FFX721@users.noreply.github.com>
2019-06-04 14:54:08 +0200

    hinzufuegen von Wuerfelicons

--- Eclipse/Monopoly/src/gui/WuerfelStage.java
+++ Eclipse/Monopoly/src/gui/WuerfelStage.java
@@ -1,11 +1,21 @@
 package gui;
 
+import java.io.File;
+import java.util.ArrayList;
+
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public class WuerfelStage extends Thread {
 
 	public void run() {
 		Stage stage = new Stage();
-		
+		ArrayList<Label> views = new ArrayList<Label>();
+		File dir = new File("/wuerfel");
+		for (File x: dir.listFiles()) {
+			ImageView icon = new ImageView(new Image(x));
+		}
 	}
 }
