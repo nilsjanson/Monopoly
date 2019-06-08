9243b8aafdbf1f3f2d29242678beaf8fc7c1e2cd
lucastheiss <lucastheiss@macbook-pro.fritz.box>
2019-06-08 12:21:42 +0200

    Javadoc added to Main

--- Eclipse/Monopoly/src/main/Main.java
+++ Eclipse/Monopoly/src/main/Main.java
@@ -3,12 +3,19 @@ package main;
 import gui.Board;
 import javafx.application.Application;
 import javafx.stage.Stage;
-
+/**
+ * Diese Klasse startet die Anwendung.
+ * In dieser Klasse befindet sich die Main-Methode.
+ *
+ */
 public class Main extends Application {
 	Stage prime;
 	Board board;
-	//Test Commit
 	
+	/**
+	 * Startet die Anwendung.
+	 * @param args wird nicht beachtet, ist nur vorhanden wegen Standardsignatur der Main-Methode
+	 */
 	public static void main(String...args) {
 		launch();
 	}
