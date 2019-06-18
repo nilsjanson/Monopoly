1e726cf67cbacd56dbebbf57e6cac05fcf12b1d5
Faier <Faier@143.93.95.128>
2019-06-18 12:14:46 +0200

    Hinzufuegen der Spielernameabfrage

--- Eclipse/Monopoly/src/gui/Board.java
+++ Eclipse/Monopoly/src/gui/Board.java
@@ -14,6 +14,7 @@ import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
@@ -44,7 +45,7 @@ public class Board {
 	ImageView player1;
 	double playerStartPositionX;
 	double playerStartPositionY;
-	public Semaphore actionSeamphore ;
+	public Semaphore actionSeamphore;
 
 	public Board(Stage prime) {
 		mediaPlayer.play();
@@ -314,21 +315,28 @@ public class Board {
 		Button zwei = new Button("2 Spieler");
 		Button drei = new Button("3 Spieler");
 		Button vier = new Button("4 Spieler");
-		
-		 zwei.setOnAction(new EventHandler<ActionEvent>() {
-	            @Override
-	            public void handle(ActionEvent event) {
-	               startGame(2);
-	            }
-	        });
-		drei.setOnAction(e -> startGame(3));
-		vier.setOnAction(e -> startGame(4));
-		
+
 		butStyle(zwei, drei, vier);
 		hbox.getChildren().addAll(zwei, drei, vier);
 		hbox.setSpacing(10);
+		hbox.setStyle("-fx-padding:1em");
 		hbox.setAlignment(Pos.CENTER);
-		vbox.getChildren().addAll(logo, hbox);
+		
+		HBox playerNames = new HBox();
+		TextField name = new TextField();
+		name.setOnAction(e-> sendPlayerName(name.getText()));
+		Label playerName = new Label("Ihr Spielername:");
+		playerName.setStyle("-fx-background-color:lightgreen; -fx-padding:0.5em; -fx-text-fill: black; -fx-font-size:20px;");
+		textFieldStyle(name);
+		playerNames.getChildren().addAll(playerName,name);
+		playerNames.setAlignment(Pos.CENTER);
+		
+		zwei.setOnAction(e -> startGame(2,name.getText()));
+		drei.setOnAction(e -> startGame(3,name.getText()));
+		vier.setOnAction(e -> startGame(4,name.getText()));
+		
+		
+		vbox.getChildren().addAll(logo, hbox,playerNames);
 		Scene scene = new Scene(vbox);
 		vbox.autosize();
 		controlWelcome(scene);
@@ -339,13 +347,23 @@ public class Board {
 		welcome.centerOnScreen();
 	}
 	
-	private void startGame(int player) {
+	private void sendPlayerName(String name) {
+		System.out.println("SendNilsPlayerName("+name+");");
+	}
+
+	private void startGame(int player,String playername) {
 		spieler = player;
 		actionSeamphore.release();
 		createBoard();
-		
 	}
 
+	private void textFieldStyle(TextField... x) {
+		for (TextField field : x) {
+			field.setStyle("-fx-border-color: black; -fx-control-inner-background: lightgreen; -fx-font-size: 2em;");
+		}
+		// vbox.setStyle("-fx-background-color: rgb(" + 192 + "," + 254 + ", " + 213 + ");");
+	}
+	
 	private void butStyle(Button... x) {
 		for (Button but : x) {
 			but.setStyle("-fx-border-color: black; -fx-background-color: lightgreen; -fx-font-size: 2em;");
