25a1c778a48be28eb1ab6f4d21c0598303b5f9f9
FFX721 <49677342+FFX721@users.noreply.github.com>
2019-06-18 21:20:36 +0200

    Anpassung der Position der WuerfelStage

--- Eclipse/Monopoly/src/gui/WuerfelStage.java
+++ Eclipse/Monopoly/src/gui/WuerfelStage.java
@@ -112,7 +112,7 @@ public class WuerfelStage {
 		stage.initStyle(StageStyle.UNDECORATED);
 		stage.show();
 		stage.setAlwaysOnTop(true);
-		stage.setX((max / 2) - (stage.getWidth()));
+		stage.setX((max / 2) - (stage.getWidth()+(stage.getWidth()*.35)));
 		stage.setY(max / 2);
 	}
 
