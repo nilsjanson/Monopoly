34ad610f8ca8340e4df63e44cab639cacff250ab
FFX721 <49677342+FFX721@users.noreply.github.com>
2019-06-18 17:49:18 +0200

    Anpassung der Lobby

--- Eclipse/Monopoly/src/gui/Lobby.java
+++ Eclipse/Monopoly/src/gui/Lobby.java
@@ -1,12 +1,19 @@
 package gui;
 
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Lobby {
 
 	Lobby(String...player) {
 		Stage prime = new Stage();
-		
+		VBox vbox = new VBox();
+		vbox.setStyle("-fx-background-color: rgb(" + 192 + "," + 254 + ", " + 213 + ");");
+		for (String x: player) {
+			Label lobby = new Label(x);
+			vbox.getChildren().add(lobby);
+		}
 		prime.show();
 	}
 
