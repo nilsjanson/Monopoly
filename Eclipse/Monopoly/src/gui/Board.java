a0d41226ad29458fa471ab1bb87dafe1e6c4ac1a
FFX721 <49677342+FFX721@users.noreply.github.com>
2019-06-20 16:37:24 +0200

    unused imports

--- Eclipse/Monopoly/src/gui/Board.java
+++ Eclipse/Monopoly/src/gui/Board.java
@@ -7,7 +7,6 @@ import java.util.concurrent.Semaphore;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
-import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
