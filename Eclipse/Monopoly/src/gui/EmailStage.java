5cdfed0f862938e4e17019bded5308ce8febb557
nilsjanson <nils.janson@th-bingen.de>
2019-06-21 19:24:58 +0200

    Revert "Synchronisation"
    This reverts commit 77c51cd6149c8033a25dec6d85690737e86f16db.

--- Eclipse/Monopoly/src/gui/EmailStage.java
+++ Eclipse/Monopoly/src/gui/EmailStage.java
@@ -1,7 +1,5 @@
 package gui;
 
-import java.net.URL;
-
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
@@ -9,8 +7,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.VBox;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -22,40 +20,32 @@ public class EmailStage extends Application {
 	String von;
 	String inhalt;
 
-	final URL resource = getClass().getResource("/sounds/mailSound.wav");
-	final Media media = new Media(resource.toString());
-	final MediaPlayer mediaPlayer = new MediaPlayer(media);
-	
-	
 	EmailStage(Board board, String betreff, String von, String inhalt) {
 		this.betreff = betreff;
 		this.von = von;
 		this.inhalt = inhalt;
-		this.board = board;
-		start(board.prime);
+		this.board=board;
 	}
 
 	@Override
-	public void start(Stage primaryStage) {
-		mediaPlayer.play();
+	public void start(Stage primaryStage) throws Exception {
 		Stage email = new Stage();
 		VBox vbox = new VBox();
 		vbox.setAlignment(Pos.CENTER);
 		Scene scene = new Scene(vbox);
-		Label head = new Label("Sie haben Post!");
-		head.setStyle("-fx-font-size: 6em; -fx-font-color: white;");
-		Label betref = new Label("Betreff: " + betreff);
-		Label vo = new Label("Von: " + von);
-		Label absatz = new Label("");
-		Label inhal = new Label(inhalt);
-		labelStyle(betref, vo, inhal);
-		vboxStyle(vbox);
-		vbox.autosize();
-		vbox.getChildren().addAll(head,betref, vo, absatz, inhal);
+		Label betref = new Label(betreff);
+		Label vo = new Label(von);
+		Label inhal=new Label(inhalt);
+		Rectangle rahmen = new Rectangle();
+		rahmen.setStroke(Color.BLACK);
+		rahmen.setWidth(email.getWidth() - 10);
+		rahmen.setHeight(email.getHeight() - 10);
+		vbox.getChildren().addAll(betref, vo, inhal, rahmen);
 		email.setScene(scene);
 		email.initStyle(StageStyle.UNDECORATED);
 		email.initModality(Modality.APPLICATION_MODAL);
 		email.show();
+		
 
 		scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
 			@Override
@@ -73,15 +63,5 @@ public class EmailStage extends Application {
 				}
 			}
 		});
-	}
-
-	void labelStyle(Label... labels) {
-		for (Label x : labels) {
-			x.setStyle("-fx-font-size: 2em;");
-		}
-	}
-
-	void vboxStyle(VBox vbox) {
-		vbox.setStyle("-fx-background-color: rgb( 192,254,213); -fx-border-color: black;");
 	}
 }
